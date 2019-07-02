33e86c66f1a643c7dce21d5ef77ff86666e0f15c
Konstantinos Dalkafoukis <konstantinos.dalkafoukis@miceanddice.co.uk>
2019-07-02 17:08:30 +0100

    helloworld tensorflow

---
+++ tensorflow_helloworld/helloworld.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+print(tf.version.VERSION)
+print(tf.keras.__version__)
